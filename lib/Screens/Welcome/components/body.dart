db04be1d0e2721f536bac3c7c6a6f690f4fdf4e5
arcznze <87095931+arcznze@users.noreply.github.com>
2023-04-12 22:32:29 -0400

    added welcome page.

---
+++ lib/Screens/Welcome/components/body.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size; //Nos da las dimensiones totales de nuestra pantalla.
+    return Container(
+      height: size.height,
+      width: double.infinity,
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+            top: 0,
+            left: 0,
+            child: Image.asset(
+              "assets/images/topCircle.png", 
+              width: size.width,
+              ),
+            )
+        ],
+      ),
+    );
+  }
+}
