a3dae5011e4d54803dd644e5a256649f2ea22a7e
arcznze <87095931+arcznze@users.noreply.github.com>
2023-04-13 23:59:55 -0400

    added signup button.

--- lib/Screens/Welcome/components/background.dart
+++ lib/Screens/Welcome/components/background.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:grocery_tracker_app/components/rounded_button.dart';
 import 'package:grocery_tracker_app/constants.dart';
 
 class Background extends StatelessWidget {
   final Widget child;
   const Background({
-    super.key, required this.child,
+    super.key,
+    required this.child,
   });
 
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size; 
+    Size size = MediaQuery.of(context).size;
     return Container(
       height: size.height,
       width: double.infinity,
@@ -21,48 +23,37 @@ class Background extends StatelessWidget {
             top: 0,
             left: 0,
             child: Image.asset(
-              "assets/images/topCircle.png", 
+              "assets/images/topCircle.png",
               width: size.width,
-              ),
             ),
-            Positioned(
+          ),
+          Positioned(
               bottom: 0,
               left: 0,
               child: Image.asset(
                 "assets/images/bottonCircle.png",
                 width: size.width,
-              )
-            ),
-            Positioned(
+              )),
+          Positioned(
               right: 0,
               child: Image.asset(
                 "assets/images/middleCircle.png",
                 width: size.width,
-                )
-              ),
-            Positioned(
+              )),
+          Positioned(
               child: SvgPicture.asset(
-                "assets/icons/topIcon.svg",
-                width: size.width,
-                )
-              ),
-              Positioned(
-                child: SvgPicture.asset(
-                  "assets/icons/bottomIcon.svg",
-                  width: size.width,
-                  )
-              ),
-              TextButton(
-                style: TextButton.styleFrom(
-                  padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 40),
-                  backgroundColor: kPrimaryColor,
-                  ),
-                  onPressed: (){}, 
-                  child: const Text(
-                    "LOGIN", 
-                    style: TextStyle(color: Colors.white),
-                    ),
-                  ),
+            "assets/icons/topIcon.svg",
+            width: size.width,
+          )),
+          Positioned(
+              child: SvgPicture.asset(
+            "assets/icons/bottomIcon.svg",
+            width: size.width,
+          )),
+          RoundedButton(
+            text: "LOGIN",
+            press: () {},
+          ),
         ],
       ),
     );
---
+++ lib/components/rounded_button.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+import '../constants.dart';
+
+class RoundedButton extends StatelessWidget {
+  final String text;
+  final Function press;
+  final Color color, textColor;
+  const RoundedButton({
+    super.key, 
+    required this.text, 
+    required this.press, 
+    this.color = kPrimaryColor, 
+    this.textColor = Colors.black,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 40),
+      margin: EdgeInsets.symmetric(vertical: 20),
+      width: size.width * 0.8,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(24),
+        child: TextButton(
+          style: TextButton.styleFrom(
+            padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 40),
+            backgroundColor: color,
+            ),
+            onPressed: (){}, 
+            child: Text(
+              text, 
+              style: TextStyle(color: textColor),
+              ),
+            ),
+      ),
+    );
+  }
+}
