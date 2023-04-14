4872d3d449e469389edbdf25e61a63e9916ba42c
arcznze <87095931+arcznze@users.noreply.github.com>
2023-04-13 23:03:38 -0400

    change welcome page.

---
+++ lib/Screens/Welcome/components/background.dart
@@ -0,0 +1,66 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class Background extends StatelessWidget {
+  final Widget child;
+  const Background({
+    super.key, required this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size; 
+    return Container(
+      height: size.height,
+      width: double.infinity,
+      child: Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          Positioned(
+            top: 0,
+            left: 0,
+            child: Image.asset(
+              "assets/images/topCircle.png", 
+              width: size.width,
+              ),
+            ),
+            Positioned(
+              bottom: 0,
+              left: 0,
+              child: Image.asset(
+                "assets/images/bottonCircle.png",
+                width: size.width,
+              )
+            ),
+            Positioned(
+              child: Image.asset(
+                "assets/images/middleCircle.png",
+                width: size.width,
+                )
+              ),
+            Positioned(
+              child: SvgPicture.asset(
+                "assets/icons/topIcon.svg",
+                width: size.width,
+                )
+              ),
+              Positioned(
+                child: SvgPicture.asset(
+                  "assets/icons/bottomIcon.svg",
+                  width: size.width,
+                  )
+              ),
+              TextButton(
+                style: TextButton.styleFrom(
+                  padding: EdgeInsets.symmetric(vertical: 20, horizontal: 40)
+                  ),
+                  onPressed: (){}, 
+                  child: Text("LOGIN"),
+                  ),
+        ],
+      ),
+    );
+  }
+}
--- lib/Screens/Welcome/components/body.dart
+++ lib/Screens/Welcome/components/body.dart
@@ -1,24 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:grocery_tracker_app/Screens/Welcome/components/background.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
 
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size; //Nos da las dimensiones totales de nuestra pantalla.
-    return Container(
-      height: size.height,
-      width: double.infinity,
-      child: Stack(
+    return Background(
+      child: Column(
         children: <Widget>[
-          Positioned(
-            top: 0,
-            left: 0,
-            child: Image.asset(
-              "assets/images/topCircle.png", 
-              width: size.width,
-              ),
-            )
         ],
       ),
     );
   }
 }
+
